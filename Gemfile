728af4c48aae139c2f30903890b006c79208a0f5
tomanistor <toma.nistor@gmail.com>
2017-06-05 00:53:52 -0700

    Added form builder and completed post creation

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'haml'
 gem "haml-rails"
 gem "paperclip", "~> 5.0.0"
 gem 'sass-rails', '~> 5.0'
+gem 'simple_form'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -4,10 +4,10 @@ RSpec.feature "CreatingPosts", type: :feature do
   scenario 'can create a post' do
     visit '/'
     click_link 'New Post'
-    attach_file('Image', "spec/files/images/coffee.jpg")
-    fill_in 'Caption', with: 'nom nom nom #coffeetime'
+    attach_file('Image', "spec/files/images/corgi.jpg")
+    fill_in 'Caption', with: 'silly dog #corgi'
     click_button 'Create Post'
-    expect(page).to have_content('#coffeetime')
-    expect(page).to have_css("img[src*='coffee.jpg']")
+    expect(page).to have_content('#corgi')
+    expect(page).to have_css("img[src*='corgi.jpg']")
   end
 end
